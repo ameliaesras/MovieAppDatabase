2518df64c2461d7013031f141b7e2ed1093f6b96
Amel <esramelia21@gmail.com>
2023-03-23 12:33:45 +0700

    push commit

--- TMDB/Controller/ReviewViewController.swift
+++ TMDB/Controller/ReviewViewController.swift
@@ -19,7 +19,6 @@ class ReviewViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
 
